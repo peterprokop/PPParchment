07ca96bf640cb6b7e1f56bdd296b6496eddaf3e0
Peter Prokop <prokop.petr@gmail.com>
2014-08-08 13:20:44 +0400

    Initial Commit

---
+++ PPParchmentDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PPParchmentDemo
+//
+//  Created by Peter Prokop on 08/08/14.
+//  Copyright (c) 2014 PeterProkop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PPParchmentDemoTests/PPParchmentDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  PPParchmentDemoTests.m
+//  PPParchmentDemoTests
+//
+//  Created by Peter Prokop on 08/08/14.
+//  Copyright (c) 2014 PeterProkop. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PPParchmentDemoTests : XCTestCase
+
+@end
+
+@implementation PPParchmentDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
